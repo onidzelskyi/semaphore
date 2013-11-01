12bcb1d0d34a384a816dc1ff1e7d29a54c3a31ce
onidzelskyi <alexey.education@gmail.com>
2013-11-01 14:29:40 +0200

    Revert "Delete platform preprocessing macros. Rewrite semaphores as named. Delete unnamed semaphore"
    This reverts commit 48421b443a7f4bac77c246bade70da3131d6dc80.

--- semaphore/main.cpp
+++ semaphore/main.cpp
@@ -6,14 +6,10 @@
 //  Copyright (c) 2013 Alexey. All rights reserved.
 //
 
-#include <stdio.h>
-#include <fcntl.h>
-#include <sys/stat.h>
+#include <iostream>
 #include <semaphore.h>
 #include <errno.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <pthread.h>
 
 
 sem_t* mutex_A;
@@ -28,41 +24,52 @@ char* buf = 0;
 void* handler_A(void*) {
     buf = (char*)line_A;
     sem_post(mutex_A);
-    pthread_exit(0);
+    exit(0);
 }
 
 
 void* handler_B(void*) {
     sem_wait(mutex_A);
     printf("%s", buf);
-    pthread_exit(0);
+    exit(0);
 }
 
 
 int main(int argc, const char * argv[])
 {
-/*
 #ifdef __APPLE__
     if((mutex_A=sem_open("/sem_A",O_CREAT,0777,0))==SEM_FAILED) {
         perror("sem_open");
     }
 #elif __linux___
-    if (sem_init(mutex_A, 0, 0)==-1) {
+    if (sem_init(mutex_A, 0, 0)) {
         perror("Error:");
     }
 #elif _WIN32
 #endif
-*/
-    if((mutex_A=sem_open("/sem_A",O_CREAT,0777,0))==SEM_FAILED) {
-        perror("sem_open");
-    }
     pthread_create(&thread_A, 0, handler_A, 0);
     pthread_create(&thread_B, 0, handler_B, 0);
 
     pthread_join(thread_A, 0);
     pthread_join(thread_B, 0);
 
-    sem_destroy(mutex_A);
+    /*
+    sem_t* sem = sem_open("/sem1",O_CREAT,0777,10);
+    if(sem==SEM_FAILED) {
+        perror("sem_open");
+    } else  {
+//        sem_post(sem);
+        int ret = sem_wait(sem);
+        if(ret) {
+            perror("sem_open");
+        } else {
+            std::cout << "Hello, World!\n";
+        }
+    }
+     */
+    while (1) {
+        sleep(1);
+    }
     return 0;
 }
 
